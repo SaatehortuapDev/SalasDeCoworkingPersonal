576abb9029b5db8b7142a66ad91bbb974a37350d
alrago24 <alinson_04@hotmail.com>
2026-04-11 14:01:18 -0500

    Creación de proyecto

---
+++ src/main/java/com/dos/salasDeCoworking/SalasDeCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.dos.salasDeCoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasDeCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalasDeCoworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dos/salasDeCoworking/SalasDeCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dos.salasDeCoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasDeCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
